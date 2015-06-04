54c2b729fd0ec12f9cd4f211d9a27e8988983069
kireed <kireed@g.hmc.edu>
2015-06-04 09:28:18 -0700

    Updated settings to work with MySQL database

--- dynamicfiltersite/dynamicfiltersite/settings.py
+++ dynamicfiltersite/dynamicfiltersite/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'dynamicfiltersite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dynamicfilter2',
+        'USER': 'webapp',
+        'PASSWORD': 'dynamicfilter',
     }
 }
 
@@ -87,7 +89,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
