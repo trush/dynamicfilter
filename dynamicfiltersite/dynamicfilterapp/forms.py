ca7d3f89603face7f71282e6fdf59b8457005f2c
kireed <kireed@g.hmc.edu>
2015-06-04 16:41:20 -0700

    Form rewritten using Django class, still not working
    URLs are not redirecting properly to process form input.

--- dynamicfiltersite/dynamicfilterapp/forms.py
+++ dynamicfiltersite/dynamicfilterapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from models import RestaurantPredicate
 
 class WorkerForm(forms.Form):
     answer = forms.NullBooleanField(label='answer')
--- dynamicfiltersite/dynamicfilterapp/views.py
+++ dynamicfiltersite/dynamicfilterapp/views.py
@@ -1,54 +1,65 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from models import Restaurant, RestaurantPredicate, Task
 from django.db import models
 from .forms import WorkerForm
 
 def index(request):
-	return render(request, 'dynamicfilterapp/index.html')
+    return render(request, 'dynamicfilterapp/index.html')
 
 def answer_question(request):
-	context = {}
 
-	# get all the RestaurantPredicates in the database
-	restaurantPredicates = RestaurantPredicate.objects.all()
+    toBeAnswered = find_unanswered_predicate()
 
-	# if there aren't any RestaurantPredicates, set error message for template and return
-	if not restaurantPredicates.exists():
-		context['errorMessage'] = "There are no existing predicates."
-		return render(request, 'dynamicfilterapp/answer_question.html', context)
+    if toBeAnswered == None:
+        return render(request, 'dynamicfilterapp/index.html')
+        # TODO make a page informing that there are no predicates to be answered (instead
+        # of redirecting to index)
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = WorkerForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            task = Task(restaurantPredicate = toBeAnswered, answer = form.cleaned_data['answer'], workerID = 000)
+            #TODO fill in real worker ID, not 000
+            task.save()
+            # redirect to a new URL:
+            return HttpResponseRedirect('/index/')
 
-	# find the first unanswered predicate if it exists
-	toBeAnswered = None
-	assert restaurantPredicates.exists()
-	for predicate in restaurantPredicates:
-		if predicate.value == None:
-	 		toBeAnswered = predicate
-	 		break
-	
-	# if there was no unanswered predicate, set error message for template and return
-	if toBeAnswered == None:
-		context['errorMessage'] = "There are no unanswered predicates."
-		return render(request, 'dynamicfilterapp/answer_question.html', context)
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = WorkerForm()
 
-	# if there was an unanswered predicate, it in the context dictionary
-	context['predicate'] = toBeAnswered
-
-	return render(request, 'dynamicfilterapp/answer_question.html', context)
+    return render(request, 'dynamicfilterapp/answer_question.html', {'form': form, 'predicate': toBeAnswered})
 
 def completed_question(request):
-	return HttpResponse("You answered a question!")
+    return render(request, 'dynamicfilterapp/completed_question.html')
 
-def enter_task(request):
-	# make a new task object using the worker-entered data and save it
-	# to the database
-	task = Task(restaurantPredicate = 
+def find_unanswered_predicate():
+    """
+    A helper for the answer_question method. Finds the first unanswered predicate and returns it.
+    Returns None if there are no unanswered predicates.
+    """
+    # get all the RestaurantPredicates in the database
+    restaurantPredicates = RestaurantPredicate.objects.all()
 
-	if request.POST['answer'] == "yes":
+    # if there aren't any RestaurantPredicates, set error message for template and return
+    if not restaurantPredicates.exists():
+        return None
 
-	
+    # find the first unanswered predicate if it exists
+    nextUnanswered = None
+    assert restaurantPredicates.exists()
+    for predicate in restaurantPredicates:
+        if predicate.value == None:
+            nextUnanswered = predicate
+            break
+    
+    # if there was no unanswered predicate, set error message for template and return
+    if nextUnanswered == None:
+        return None
 
-	return HttpResponseRedirect(reverse('dynamicfilterapp:index'))
-
-
-
+    # if there was an unanswered predicate, it in the context dictionary
+    return nextUnanswered
