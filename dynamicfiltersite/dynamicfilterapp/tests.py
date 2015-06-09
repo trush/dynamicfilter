598622d6f047de2712e49ce1422649df079b72ee
kireed <kireed@g.hmc.edu>
2015-06-09 13:41:32 -0700

    testing git sync capabilities

--- dynamicfiltersite/dynamicfilterapp/tests.py
+++ dynamicfiltersite/dynamicfilterapp/tests.py
@@ -3,3 +3,5 @@ from django.test import TestCase
 # Create your tests here.
 from .models import Restaurant, RestaurantPredicate, Task
 from django.test.utils import setup_test_environment
+
+# I'm writing a test. I'm Kate.
