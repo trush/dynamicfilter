84031ca08fd1a14a8966b83f9b7dbc81673218af
shinaushin <shinaushin@gmail.com>
2015-06-12 11:44:46 -0700

    Added another form to process worker ID instead of dummy value 222

--- dynamicfiltersite/dynamicfilterapp/forms.py
+++ dynamicfiltersite/dynamicfilterapp/forms.py
@@ -3,3 +3,6 @@ from models import RestaurantPredicate
 
 class WorkerForm(forms.Form):
     answer = forms.NullBooleanField(label='Your answer')
+
+class IDForm(forms.Form):
+    workerID = forms.CharField(label='Worker ID')
