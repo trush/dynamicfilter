048cd0713ea31539f905b12f5c036d0d5837688c
kireed <kireed@g.hmc.edu>
2015-06-12 11:58:47 -0700

    Create three predicates automatically on the creation of a new restaurant

--- dynamicfiltersite/dynamicfilterapp/admin.py
+++ dynamicfiltersite/dynamicfilterapp/admin.py
@@ -1,26 +1,32 @@
 from django.contrib import admin
+from forms import RestaurantAdminForm
 
 # Register models so they're accessible from admin site
 from .models import Restaurant, RestaurantPredicate, Task
 
 # RestaurantPredicates should be edited from the Restaurant page
-class RestaurantPredicateInline(admin.StackedInline):
-    model = RestaurantPredicate
-    # have space for 3 RestaurantPredicates by default
-    extra = 3
-    #readonly_fields = ('value',)
+# class RestaurantPredicateInline(admin.StackedInline):
+#     model = RestaurantPredicate
+#     # have space for 3 RestaurantPredicates by default
+#     extra = 3
+#     #readonly_fields = ('value',)
 
 
 class RestaurantAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ('General Information', {'fields': ['name', 'url', 'text']}),
-        ('Address', {'fields': ['street' ,'city', 'state', 'zipCode', 'country']})
-    ]
-    list_display = ('name',)
-    inlines = [RestaurantPredicateInline]
+    form = RestaurantAdminForm
+    # fieldsets = [
+    #     ('General Information', {'fields': ['name', 'url', 'text']}),
+    #     ('Address', {'fields': ['street' ,'city', 'state', 'zipCode', 'country']})
+    # ]
+    # list_display = ('name',)
+    # inlines = [RestaurantPredicateInline]
 
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('restaurantPredicate', 'workerID', 'answer', 'completionTime')
 
+class RestaurantPredicateAdmin(admin.ModelAdmin):
+    list_display = ('question', 'restaurant')
+
 admin.site.register(Restaurant, RestaurantAdmin)
 admin.site.register(Task, TaskAdmin)
+admin.site.register(RestaurantPredicate, RestaurantPredicateAdmin)
--- dynamicfiltersite/dynamicfilterapp/forms.py
+++ dynamicfiltersite/dynamicfilterapp/forms.py
@@ -1,5 +1,20 @@
 from django import forms
-from models import RestaurantPredicate
+from models import Restaurant, RestaurantPredicate
 
 class WorkerForm(forms.Form):
     answer = forms.NullBooleanField(label='Your answer')
+
+class RestaurantAdminForm(forms.ModelForm):
+
+	class Meta:
+		model = Restaurant
+		fields = ['name', 'url', 'text', 'street']
+
+	def save(self, commit=True):
+		instance = super(RestaurantAdminForm, self).save(commit=False)
+		if commit:
+			instance.save()
+		RestaurantPredicate.objects.create(restaurant=instance, question="Do they serve Austin Shin?")
+		RestaurantPredicate.objects.create(restaurant=instance, question="Would you recommend the Kate Reed?")
+		RestaurantPredicate.objects.create(restaurant=instance, question="Are you Prof Beth?")
+		return instance
