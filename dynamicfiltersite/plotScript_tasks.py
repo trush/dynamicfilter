67f53047b97463b737fa681f7027d3a1c1561320
kireed <kireed@g.hmc.edu>
2015-07-06 13:10:09 -0700

    Generated figures for 1000 simulations test

--- dynamicfiltersite/plotScript_tasks.py
+++ dynamicfiltersite/plotScript_tasks.py
@@ -12,7 +12,14 @@ data = np.loadtxt(filename, skiprows=1, delimiter=',', dtype={'names': ('eddy nu
           'formats': (np.int, np.float, np.int, np.float)})
 
 s0 = [value0 for (value0, value1, value2, value3) in data]
+s1 = [value1 for (value0, value1, value2, value3) in data]
 s2 = [value2 for (value0, value1, value2, value3) in data]
+s3 = [value3 for (value0, value1, value2, value3) in data]
+print s1
+print s3
+
+print np.average(s0)
+print np.average(s2)
 
 fig = plt.figure()
 
@@ -26,12 +33,12 @@ plt.xlabel('Tasks Completed')
 plt.ylabel('Number of Runs')
 plt.title('Tasks to Complete all Questions (Eddy)')
 plt.savefig('test_results/eddy_histogram' + str(now.date())+ "_" + str(now.time())[:-7] + '.png')
-
+#-----------------------
 plt.clf()
 plt.cla()
 
 fig = plt.figure()
-print s2
+
 # add grid lines
 ax = fig.add_subplot(111)
 ax.grid()
@@ -42,3 +49,39 @@ plt.xlabel('Tasks Completed')
 plt.ylabel('Number of Runs')
 plt.title('Tasks to Complete all Questions (Random)')
 plt.savefig('test_results/random_histogram' + str(now.date())+ "_" + str(now.time())[:-7] + '.png')
+# #-----------------------
+# plt.clf()
+# plt.cla()
+
+# fig = plt.figure()
+
+# # add grid lines
+# ax = fig.add_subplot(111)
+# ax.grid()
+
+# plt.hist(s1, bins=10)
+
+# plt.xlabel('Correct percentage')
+# plt.ylabel('Number of Runs')
+# plt.title('Correct percentage (Eddy)')
+# plt.savefig('test_results/eddy_correct_histogram' + str(now.date())+ "_" + str(now.time())[:-7] + '.png')
+
+# #-----------------------
+# plt.clf()
+# plt.cla()
+
+# fig = plt.figure()
+
+# # add grid lines
+# ax = fig.add_subplot(111)
+# ax.grid()
+
+# plt.hist(s3, bins=10)
+
+# plt.xlabel('Correct percentage')
+# plt.ylabel('Number of Runs')
+# plt.title('Correct percentage (Random)')
+# plt.savefig('test_results/random_correct_histogram' + str(now.date())+ "_" + str(now.time())[:-7] + '.png')
+
+
+
