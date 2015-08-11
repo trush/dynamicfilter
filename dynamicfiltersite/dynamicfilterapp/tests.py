c76b2648398aeb588df1427f316e34db2b9533c0
shinaushin <shinaushin@gmail.com>
2015-08-11 11:57:29 -0700

    more test files of predicates of varying ambiguity

--- dynamicfiltersite/dynamicfilterapp/tests.py
+++ dynamicfiltersite/dynamicfilterapp/tests.py
@@ -95,7 +95,7 @@ class SimulationTest(TestCase):
         # A set of parameters for one call to many_simulations
         # The user may copy these lines to define and append more sets of parameters, which will results
         # in more calls to many_simulations
-        set1 =[ 1000, # number of simulations to be run with these parameters
+        set1 =[ 100, # number of simulations to be run with these parameters
                 20, # number of restaurants
                 [0,2,9], # indices of the questions to use (between 1 and 10 questions may be specified, with indices 0 to 9)
                 recordAggregateStats,
