eaa0bf3fd45cd58c8eb0fce34f327e41a3de3f78
hmc-cs-mpalanker <mpalaker@cs.hmc.edu>
2017-06-02 11:30:49 -0700

    Changed Single pair cost to new plotscript format

--- active/dynamicfilterapp/simulation_files/plotScript_single_cost_hist.py
+++ active/dynamicfilterapp/simulation_files/plotScript_single_cost_hist.py
@@ -8,24 +8,25 @@ import datetime as DT
 import pylab
 import sys
 
-filename = sys.argv[1]
-costs = list(np.loadtxt(filename, delimiter=','))
+def single_cost_gen(filename, dest):
+    costs = list(np.loadtxt(filename, delimiter=','))
+    #print costs
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
 
-# the histogram of the data
-n, bins, patches = ax.hist(costs, 1000, normed=1, facecolor='green')
+    # the histogram of the data
+    n, bins, patches = ax.hist(costs, 1000, normed=1, facecolor='green')
 
-bincenters = 0.5*(bins[1:]+bins[:-1])
+    bincenters = 0.5*(bins[1:]+bins[:-1])
 
-ax.set_xlabel('Cost')
-ax.set_ylabel('Frequency')
-ax.set_title('Histogram of Single IP Pair costs')
-ax.set_xlim(0, 100)
-ax.set_ylim(0, 0.3)
-ax.grid(True)
+    ax.set_xlabel('Cost')
+    ax.set_ylabel('Frequency')
+    ax.set_title('Histogram of Single IP Pair costs')
+    ax.set_xlim(0, 100)
+    ax.set_ylim(0, 0.3)
+    ax.grid(True)
 
-#plt.show()
-now = DT.datetime.now()
-plt.savefig('single_cost' + str(now.date())+ "_" + str(now.time())[:-7] + '.png')
+    #plt.show()
+    now = DT.datetime.now()
+    plt.savefig(dest + '_single_cost.png')
