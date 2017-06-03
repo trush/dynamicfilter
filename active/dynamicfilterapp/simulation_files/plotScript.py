427f5e5d4c049ccc9f06f94d5fb16027a6e64cd1
hmc-cs-mpalanker <mpalaker@cs.hmc.edu>
2017-06-02 17:14:12 -0700

    weekend!

--- active/dynamicfilterapp/simulation_files/plotScript.py
+++ active/dynamicfilterapp/simulation_files/plotScript.py
@@ -6,8 +6,30 @@ import datetime as DT
 import pylab
 import sys
 from collections import defaultdict
+import os.path
+
+def dest_resolver(dest):
+    if os.path.isfile(dest):
+        name = dest
+        num = 1
+        name += '(1)'
+        while os.path.isfile(name):
+            num+=1
+            name[-2] = num
+        return name
+    else:
+        return dest
+
 
 def hist_gen(data, dest, labels = ('',''), title='', smoothness=True):
+    """
+    Automagically generates a Histogram for you from a given list of data and a
+    destination name (ending in .png). Can additionally be passed many arguments
+        labels a touple in the format ('x-axis label', 'y-axis label')
+        title, a string title of your graph
+        smoothness, defaults true, set False to get a blocky version instead
+    """
+
     fig = plt.figure()
     ax = fig.add_subplot(111)
     if smoothness:
@@ -26,6 +48,11 @@ def hist_gen(data, dest, labels = ('',''), title='', smoothness=True):
     plt.savefig(dest)
 
 def multi_hist_gen(dataList, legendList, dest, labels=('',''), title=''):
+    """
+    Very similar to hist_gen, however takes a list of datasets and a list of
+    names of your datasets and a destination name, plots all datasets on one
+    plot in differing colors. takes in optional labels and title like before.
+    """
 
     sns.set(style="white", palette="muted", color_codes=True)
     fig = plt.figure()
@@ -42,4 +69,32 @@ def multi_hist_gen(dataList, legendList, dest, labels=('',''), title=''):
     #ax.set_xlim(100, 320)
     ax.grid(True)
 
+    plt.savefig(dest)
+
+def line_graph_gen(xpoints, ypoints, dest, labels = ('',''), title = '', stderr = []):
+    """
+    Generate a linegraph from a set of x and y points, similarly takes in
+    optional axis-labels and title, also takes in error bars
+    """
+
+    # Make the graph
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    #ax.grid()
+    if len(stderr) != 0:
+        ax.errorbar(xpoints,ypoints,yerr=stderr)
+    else:
+        ax.plot(xpoints, ypoints)
+    # Label the axes
+    plt.xlabel(labels[0])
+    plt.ylabel(labels[1])
+
+    # Title the graph
+    plt.title(title)
+
+
+    #plot curve of best fit
+    #plt.plot(queue_size, np.poly1d(np.polyfit(queue_size, num_tasks, 1))(queue_size))
+    #plt.show()
     plt.savefig(dest)
