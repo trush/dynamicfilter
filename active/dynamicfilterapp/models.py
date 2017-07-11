ef86675e82d286f7faa5217c89449f9077c2c2af
Jake Palanker <mpalanker@cs.hmc.edu>
2017-07-11 16:48:02 -0700

    Synthetic Data now works properly/has accuracy
    Synthetic Data now works properly/has accuracy 
    
    * added true_answer
    
    * updated varlist
    
    * basic synth accuracy working!
    
    * vote_grids and consensus count now work!

--- active/dynamicfilterapp/models.py
+++ active/dynamicfilterapp/models.py
@@ -6,6 +6,7 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.contrib.postgres.fields import ArrayField
 from scipy.special import btdtr
 import toggles
+import random 
 
 @python_2_unicode_compatible
 class Item(models.Model):
@@ -223,6 +224,14 @@ class IP_Pair(models.Model):
 
     # for random algorithm
     isStarted = models.BooleanField(default=False)
+
+    # for synth data:
+    true_answer = models.BooleanField(default=True)
+
+    def give_true_answer(self):
+        probability = self.predicate.trueSelectivity
+        self.true_answer = (random.random() > probability)
+        self.save(update_fields=["true_answer"])
 
     def __str__(self):
         return self.item.name + "/" + self.predicate.question.question_text
