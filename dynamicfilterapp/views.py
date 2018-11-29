9c120cbf1fb82dc11979b2744016c7eb2f6e5d15
dynamicfilter <dynamicfilter@WL-202-16.WPA.Claremont.Edu>
2018-11-28 17:34:35 -0800

    updated site to create tasks and attempt updateCounts after each vote.

--- dynamicfilterapp/views.py
+++ dynamicfilterapp/views.py
@@ -36,10 +36,12 @@ def workerForm(request):
     if not ip_pair:
         question = "no question found"
         pred_id = -1
+        item_id = -1
         item = "no item found"
     else:
         question = ip_pair.predicate.question.question_text
         pred_id = ip_pair.predicate.predicate_ID
+        item_id = ip_pair.item.name
         item = ip_pair.item.name
     # submitURL = request.GET.get("turkSubmitTo") + "/mturk/externalSubmit"
     # ip_pair = IP_Pair.objects.get(pk=1)
@@ -50,7 +52,7 @@ def workerForm(request):
     #     }
     context = {'question' : question, 
         'item': item, 
-        'pred': pred_id,
+        'pred': pred_id, 'item_id': item_id,
         'workerId':workerId, 'assignmentId':assignmentId, 'hitId' : hitId}
     return render(request, 'dynamicfilterapp/workerform.html', context)
 
@@ -59,27 +61,21 @@ def testfun(request):
 
 @xframe_options_exempt
 @csrf_exempt ###########DELETE AFTER TESTING
-def databaseTest(request):
+def databaseReset(request):
+    ''' 
+    Deletes all objects and reloads the database. SHOULD NOT BE INCLUDED IN A LIVE VERSION.
+    '''
+    Task.objects.all().delete()
+    DummyTask.objects.all().delete()
     IP_Pair.objects.all().delete()
     Predicate.objects.all().delete()
     Question.objects.all().delete()
     Item.objects.all().delete()
-    # q = Question(question_ID=1, question_text="Are you having a good day?")
-    # q.save()
-    # i1 = Item(item_ID=1, name="Wednesday", item_type=toggles.ITEM_TYPE)
-    # i1.save()
-    # p = Predicate(predicate_ID=1, question=q)
-    # p.save()
-    # ip1 = IP_Pair(item = i1, predicate = p)
-    # ip1.save()
-    # q1 = Question.objects.get(question_ID=1)
-    # context = {'question' : q1, 
-    #     'item': i1, 
-    #     'workerId':4, 'assignmentId':3, 'hitId' : 2}
 
     load_database()
-    
-    # return render(request, 'dynamicfilterapp/workerform.html', context)
+    # The render here is mostly just so the site has something to show.
+    # This view only exists to make it convenient to refresh the database
+    # on the deployed app during testing.
     return render(request, 'dynamicfilterapp/no_questions.html')
 
 @xframe_options_exempt
@@ -89,24 +85,34 @@ def vote(request):
     Page that loads in all the data from the worker task and updates the dataset
     """
     #load in data from workerForm
-
-    form_answer = (request.POST.get("workervote"))
-    print form_answer
+    workervote =  = request.POST.get("workervote")
     feedback = request.POST.get("feedback")
-    print feedback
-    workervote = form_answer
     workerId = request.POST.get("workerId")
     assignmentId = request.POST.get("assignmentId")
     hitId = request.POST.get("hitId")
     elapsed_time = request.POST.get("elapsed_time")
     pred = request.POST.get("pred")
+    pred = request.POST.get("item_id")
     question = request.POST.get("question")
+
     # submitURL = request.POST.get("submitURL")
-    # task = Task(ip_pair=questionedPair,
-    #    answer=form_answer,
-    #    workerID = workerId,
-    #    feedback=feedback)
-    #task.save()
+
+    #finds IP pair for which to record this vote
+    qItem = Item.objects.get(item_id = item_id)
+    qPred = Predicate.objects.get(predicate_ID=pred)
+    questionedPair = IP_Pair.objects.get(item=qItem,predicate=qPred)
+
+    #create a task for updating the database
+    task = Task(ip_pair=questionedPair,
+       answer=workervote,
+       workerID = workerId,
+       feedback=feedback)
+    task.save()
+
+    #update database with answer
+    updateCounts(task, questionedPair)
+
+
     context = {'question' : question, 'pred': pred,
         'workervote': workervote, 'elapsed_time': elapsed_time,
         'workerId':workerId, 'assignmentId':assignmentId, 'hitId' : hitId}
