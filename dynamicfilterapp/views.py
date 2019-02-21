074a5dee9c3fbdc90ae878b1c6ee9c2f2c0fc406
dynamicfilter <dynamicfilter@WL-210-15.WPA.Claremont.Edu>
2019-02-21 15:35:17 -0800

    there will always be dubgging pt 4

--- dynamicfilterapp/views.py
+++ dynamicfilterapp/views.py
@@ -47,7 +47,7 @@ def workerForm(request):
             item_id = ip_pair.item.item_ID
             item = ip_pair.item.name
     else:
-        incompleteIP = IP_Pair.objects.filter(isDone = False)
+        incompleteIP = IP_Pair.objects.filter(isDone = False).exists()
         if incompleteIP:
             question = "placeholder"
             pred_id = -1
