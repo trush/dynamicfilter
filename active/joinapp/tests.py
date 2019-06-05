0ff3486fa120bcbc4e657163a9aed03e9b5c3924
hanmawaung <hanmawaung2017@gmail.com>
2019-06-05 16:44:02 -0700

    Some more basic tests

--- active/joinapp/tests.py
+++ active/joinapp/tests.py
@@ -19,6 +19,38 @@ class Primary_Model_Tests(TestCase):
         prim_item.add_secondary_item(sec_item)
         self.assertIs(prim_item.check_empty(), False)
 
+    def test_primary_check_remove1(self):
+        """
+        Checking the Primary_Item properly removes itself
+        """
+
+        prim_item = Primary_Item.objects.create(item_id=1)
+        query_set = Primary_Item.objects.all()
+
+        self.assertQuerysetEqual(query_set, [prim_item], transform=lambda x: x)
+
+        prim_item.remove_self()
+        query_set = Primary_Item.objects.all()
+
+        self.assertQuerysetEqual(query_set, Primary_Item.objects.none())        
+
+    def test_primary_check_remove2(self):
+        """
+        Checking the Primary_Item properly removes itself
+        """
+
+        prim_item1 = Primary_Item.objects.create(item_id=1)
+        prim_item2 = Primary_Item.objects.create(item_id=2)
+        query_set = Primary_Item.objects.all()
+
+        self.assertQuerysetEqual(query_set, [prim_item1, prim_item2], transform=lambda x: x, ordered=False)
+
+        prim_item2.remove_self()
+        query_set = Primary_Item.objects.all()
+
+        self.assertQuerysetEqual(query_set, [prim_item1], )
+
+
 
 
 
