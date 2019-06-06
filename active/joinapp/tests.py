1f57cc6dd2ad56edf383683b9e9a1aa020b61961
hanmawaung <hanmawaung2017@gmail.com>
2019-06-06 10:19:54 -0700

    Another test+Changed item_id to be the primary key

--- active/joinapp/tests.py
+++ active/joinapp/tests.py
@@ -6,18 +6,21 @@ from django.test import TestCase
 from models import Primary_Item, Secondary_Item
 
 class Primary_Model_Tests(TestCase):
-
+    """
+    def test_primary_constructor(self):
+    """
+        
     def test_primary_check_empty(self):
         """
         Checking the check_empty function of Primary_Item Model
         """
 
         prim_item = Primary_Item.objects.create(item_id=1)
-        self.assertIs(prim_item.check_empty(), True)
+        self.assertTrue(prim_item.check_empty())
 
         sec_item = Secondary_Item.objects.create(item_id=1)
         prim_item.add_secondary_item(sec_item)
-        self.assertIs(prim_item.check_empty(), False)
+        self.assertFalse(prim_item.check_empty())
 
     def test_primary_check_remove1(self):
         """
@@ -48,7 +51,37 @@ class Primary_Model_Tests(TestCase):
         prim_item2.remove_self()
         query_set = Primary_Item.objects.all()
 
-        self.assertQuerysetEqual(query_set, [prim_item1], )
+        self.assertQuerysetEqual(query_set, [prim_item1], transform=lambda x: x)
+
+    def test_primary_many_to_many(self):
+        prim_item1 = Primary_Item.objects.create(item_id=1)
+        prim_item2 = Primary_Item.objects.create(item_id=2)
+        prim_item3 = Primary_Item.objects.create(item_id=3)
+
+        sec_item1 = Secondary_Item.objects.create(item_id=1)
+        sec_item2 = Secondary_Item.objects.create(item_id=2)
+        sec_item3 = Secondary_Item.objects.create(item_id=3)
+
+        prim_item1.add_secondary_item(sec_item1)
+        prim_item1.add_secondary_item(sec_item2)
+        prim_item1.add_secondary_item(sec_item3)
+
+        prim_item2.add_secondary_item(sec_item2)
+
+        prim_item3.add_secondary_item(sec_item1)
+        prim_item3.add_secondary_item(sec_item3)
+
+        qs1 = prim_item1.secondary_items.all()
+        qs2 = prim_item2.secondary_items.all()
+        qs3 = prim_item3.secondary_items.all()
+    
+        self.assertQuerysetEqual(qs1, [sec_item1, sec_item2, sec_item3], transform=lambda x: x, ordered=False)
+        self.assertQuerysetEqual(qs2, [sec_item2], transform=lambda x: x)
+        self.assertQuerysetEqual(qs3, [sec_item1, sec_item3], transform=lambda x: x, ordered=False)
+        
+        
+
+
 
 
 
