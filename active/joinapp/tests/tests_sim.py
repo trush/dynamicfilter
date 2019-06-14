61ace5fff7f5c33c71db790a74bdc22d29d4b77c
Amber <akampen@g.hmc.edu>
2019-06-14 10:34:11 -0700

    graphing

--- active/joinapp/tests/tests_sim.py
+++ active/joinapp/tests/tests_sim.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.test import *
 from joinapp.join_simulations import *
-from joinapp.simulation_files.plotScript import *
+from plotting import *
 
 class Simulation_Tests(TestCase):
     # def test_sim(self):
@@ -11,4 +11,4 @@ class Simulation_Tests(TestCase):
     def test_multi_sim(self):
         sim = JoinSimulation()
         results = sim.run_multi_sims()
-        hist_gen(results[4], "joinapp/simulation_files/test1.png", labels = ('time','frequency'), title='Testiing Itemwise Join', xRange=(None,None), yRange=(None,None), smoothness=True)
+        hist_gen(results[4], "joinapp/simulation_files/test2.png", labels = ('time','frequency'), title='Testing Itemwise Join', xRange=(None,None), yRange=(None,None), smoothness=True)
