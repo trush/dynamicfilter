ffb38590f26593723e532b072071da1305b3d041
Summer 19 <summer19@Summers-Mac-mini.local>
2019-06-17 11:05:55 -0700

    resolving weird merge stuff

--- active/joinapp/toggles.py
+++ active/joinapp/toggles.py
@@ -5,13 +5,8 @@ PRIMARY_LIST = ''#TODO hotel list
 REAL_DATA_CSV = '/simulation_files/CLEANED_ROUND1.csv'#TODO MTURK data CSV
 
 #_____________________________ For Synthetic Data _____________________________#
-<<<<<<< HEAD
-NUM_PRIM_ITEMS = 20
-NUM_SEC_ITEMS = 50
-=======
 NUM_PRIM_ITEMS = 90
 NUM_SEC_ITEMS = 70
->>>>>>> ed78c46946498fe156885b491f45e5806f646e7c
 HAVE_SEC_LIST = True #Do we start with the secondary list populated yes/no  <not currently in use?
 FAKE_SEC_ITEM_LIST = ["Fake 1; " + str(NUM_SEC_ITEMS+1), "Fake 2; " + str(NUM_SEC_ITEMS+2), "Fake 3; " + str(NUM_SEC_ITEMS+3), "Fake 4; " + str(NUM_SEC_ITEMS+4), "Fake 5; " + str(NUM_SEC_ITEMS+5)] #fake secondary items to choose from
 
@@ -32,11 +27,7 @@ REAL_DATA = False # real or synthetic data
 # 2 = pre-join filtered join
 JOIN_TYPE = 1
 NUM_WORKERS = 200 # number of distinct workers
-<<<<<<< HEAD
-NUM_SIMS = 5 # number of simulations to run
-=======
 NUM_SIMS = 100 # number of simulations to run
->>>>>>> ed78c46946498fe156885b491f45e5806f646e7c
 PROB_CHOOSING_TRUE_SEC_ITEM = 0.95
 
 # ________ Consensus Settings _________#
