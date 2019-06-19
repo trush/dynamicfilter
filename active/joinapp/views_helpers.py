12848af6752046b7f302ac900ae949523eae94db
hanmawaung <hanmawaung2017@gmail.com>
2019-06-19 14:38:56 -0700

    fixed most influential

--- active/joinapp/views_helpers.py
+++ active/joinapp/views_helpers.py
@@ -148,7 +148,7 @@ def choose_task_sec_pred(worker):
     # only secondary items that haven't reached consensus but match at least one primary item
     sec_items_left = SecondaryItem.objects.filter(second_pred_result=None).exclude(matches_some = False)
     if toggles.SEC_INFLUENTIAL is True:
-        sec_item = sec_items_left.order_by('num_prim_items').first() # item related to the most primary items
+        sec_item = sec_items_left.order_by('-num_prim_items').first() # item related to the most primary items
         sec_pred_task = SecPredTask.objects.get_or_create(secondary_item=sec_item)[0]
         # choose new secondary item if worker has worked on it
         while worker in sec_pred_task.workers.all():
