fc50abc5927290c74ea31467ff1eb9eeb85b4937
hanmawaung <hanmawaung2017@gmail.com>
2019-06-20 16:17:05 -0700

    Num prims left and the corresponding bugs

--- active/joinapp/toggles.py
+++ active/joinapp/toggles.py
@@ -5,16 +5,16 @@ PRIMARY_LIST = ''#TODO hotel list
 REAL_DATA_CSV = '/simulation_files/CLEANED_ROUND1.csv'#TODO MTURK data CSV
 
 #_____________________________ For Synthetic Data _____________________________#
-NUM_PRIM_ITEMS = 50
-NUM_SEC_ITEMS = 30
+NUM_PRIM_ITEMS = 90
+NUM_SEC_ITEMS = 70
 HAVE_SEC_LIST = False #Do we start with the secondary list populated yes/no 
 FAKE_SEC_ITEM_LIST = ["Fake 1; " + str(NUM_SEC_ITEMS+1), "Fake 2; " + str(NUM_SEC_ITEMS+2), "Fake 3; " + str(NUM_SEC_ITEMS+3), "Fake 4; " + str(NUM_SEC_ITEMS+4), "Fake 5; " + str(NUM_SEC_ITEMS+5)] #fake secondary items to choose from
 PJF_LIST = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 # used in syn_load_find_pairs_tasks
 # to determine how many secondary items each primary item is going to be matched with 
-MEAN_SEC_PER_PRIM = 5
-SD_SEC_PER_PRIM = 3
+MEAN_SEC_PER_PRIM = 15
+SD_SEC_PER_PRIM = 0
 PROB_NONE_SECONDARY = 0
 
 #________ For Real Data ________#
@@ -28,8 +28,8 @@ REAL_DATA = False # real or synthetic data
 # 2 = pre-join filtered join
 JOIN_TYPE = 1
 NUM_WORKERS = 200 # number of distinct workers
-NUM_SIMS = 1 # number of simulations to run
-PROB_CHOOSING_TRUE_SEC_ITEM = 0.95
+NUM_SIMS = 10 # number of simulations to run
+PROB_CHOOSING_TRUE_SEC_ITEM = 1
 SIMULATE_TIME = False
 TIME_STEP = 5
 
@@ -49,7 +49,7 @@ UPDATE_ON_CONSENSUS = False #decides whether the task statistics are updated for
 JF_AMBIGUITY = 0.1
 JF_SELECTIVITY = 0.1
 SEC_PRED_SELECTIVITY = 0.5 
-SEC_PRED_AMBIGUITY = 0.3
+SEC_PRED_AMBIGUITY = 0
 JOIN_COND_AMBIGUITY = 0 
 JOIN_COND_SELECTIVITY = 0.8 # given that these pairs were created by the crowd
 PJF_SELECTIVITY_PRIMARY = 0.1 # PJF selectivity for primary list 
