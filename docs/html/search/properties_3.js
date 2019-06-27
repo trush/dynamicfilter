54e89b4dfcb1f8477815e3a98f0c2f1bc296f4e5
rebeccaqin <51134963+rebeccaqin@users.noreply.github.com>
2019-06-27 15:40:29 -0700

    Delete properties_3.js

--- docs/html/search/properties_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['total_5fvotes',['total_votes',['../classdynamicfilterapp_1_1models_1_1_i_p___pair.html#a6de192eaff22c17d7eba33f9846d5cc3',1,'dynamicfilterapp::models::IP_Pair']]]
-];
