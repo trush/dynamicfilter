c3c5e735fa1730e3fe7c5edd49cd9cf4143beb9f
rebeccaqin <51134963+rebeccaqin@users.noreply.github.com>
2019-06-27 15:27:08 -0700

    Delete classes_b.js

--- docs/html/search/classes_b.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['worker',['Worker',['../classjoinapp_1_1models_1_1task__management__models_1_1_worker.html',1,'joinapp::models::task_management_models']]],
-  ['workerform',['WorkerForm',['../classdynamicfilterapp_1_1forms_1_1_worker_form.html',1,'dynamicfilterapp::forms']]],
-  ['workerid',['WorkerID',['../classdynamicfilterapp_1_1models_1_1_worker_i_d.html',1,'dynamicfilterapp::models']]]
-];
