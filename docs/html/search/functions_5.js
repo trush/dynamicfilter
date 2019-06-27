d42d8ccb43167873a04577f52963b9d55cd580c1
rebeccaqin <51134963+rebeccaqin@users.noreply.github.com>
2019-06-27 15:34:16 -0700

    Delete functions_5.js

--- docs/html/search/functions_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['evaluate',['evaluate',['../classdynamicfilterapp_1_1models_1_1_join.html#aced24bb06eb985f2929181e535212ffb',1,'dynamicfilterapp.models.Join.evaluate()'],['../namespaceoldjoin.html#a8b76cd232f566a0726e27b9e31cdd045',1,'oldjoin.evaluate()']]]
-];
