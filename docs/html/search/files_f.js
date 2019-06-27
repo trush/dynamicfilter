924e896ff660ffd6e5ac70d8af03d456e6e21380
rebeccaqin <51134963+rebeccaqin@users.noreply.github.com>
2019-06-27 15:31:15 -0700

    Delete files_f.js

--- docs/html/search/files_f.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['task_5fmanagement_5fmodels_2epy',['task_management_models.py',['../task__management__models_8py.html',1,'']]],
-  ['test_5fsimulations_2epy',['test_simulations.py',['../test__simulations_8py.html',1,'']]],
-  ['tests_5fmodels_2epy',['tests_models.py',['../tests__models_8py.html',1,'']]],
-  ['tests_5fsim_2epy',['tests_sim.py',['../tests__sim_8py.html',1,'']]],
-  ['toggles_2epy',['toggles.py',['../dynamicfilterapp_2toggles_8py.html',1,'(Global Namespace)'],['../joinapp_2toggles_8py.html',1,'(Global Namespace)']]],
-  ['toggles_5finfo_2edox',['toggles_info.dox',['../toggles__info_8dox.html',1,'']]]
-];
