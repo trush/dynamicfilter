cb1745661a3bfc7581408e8c809055b3bc6e0751
rebeccaqin <51134963+rebeccaqin@users.noreply.github.com>
2019-06-27 15:42:45 -0700

    Delete variables_17.js

--- docs/html/search/variables_17.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['yes_5fvotes',['yes_votes',['../classjoinapp_1_1models_1_1task__management__models_1_1_j_f_task.html#ab50f50bde4c2c1524460c48def8f27f3',1,'joinapp.models.task_management_models.JFTask.yes_votes()'],['../classjoinapp_1_1models_1_1task__management__models_1_1_join_pair_task.html#ab50f50bde4c2c1524460c48def8f27f3',1,'joinapp.models.task_management_models.JoinPairTask.yes_votes()'],['../classjoinapp_1_1models_1_1task__management__models_1_1_sec_pred_task.html#ab50f50bde4c2c1524460c48def8f27f3',1,'joinapp.models.task_management_models.SecPredTask.yes_votes()']]]
-];
