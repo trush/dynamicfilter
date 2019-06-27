ebfdb6e87ed3a6ee5b1e5df6fde3424a86a562c1
Summer 19 <summer19@Summers-Mac-mini.local>
2019-06-27 10:23:16 -0700

    tried to do some simulations

--- active/joinapp/toggles.py
+++ active/joinapp/toggles.py
@@ -5,8 +5,8 @@ PRIMARY_LIST = ''#TODO hotel list
 REAL_DATA_CSV = '/simulation_files/CLEANED_ROUND1.csv'#TODO MTURK data CSV
 
 #_____________________________ For Synthetic Data _____________________________#
-NUM_PRIM_ITEMS = 20
-NUM_SEC_ITEMS = 20
+NUM_PRIM_ITEMS = 90
+NUM_SEC_ITEMS = 70
 HAVE_SEC_LIST = True #Do we start with the secondary list populated yes/no 
 FAKE_SEC_ITEM_LIST = [str(NUM_SEC_ITEMS+1), str(NUM_SEC_ITEMS+2), str(NUM_SEC_ITEMS+3), str(NUM_SEC_ITEMS+4), str(NUM_SEC_ITEMS+5), str(NUM_SEC_ITEMS+6), str(NUM_SEC_ITEMS+7), str(NUM_SEC_ITEMS+8), str(NUM_SEC_ITEMS+9), str(NUM_SEC_ITEMS+10)] #fake secondary items to choose from
 PJF_LIST = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -15,13 +15,8 @@ CHANCE_MORE_THAN_7 = 0.7
 
 # used in syn_load_find_pairs_tasks
 # to determine how many secondary items each primary item is going to be matched with 
-<<<<<<< HEAD
 MEAN_SEC_PER_PRIM = 7
 SD_SEC_PER_PRIM = 0
-=======
-MEAN_SEC_PER_PRIM = 5
-SD_SEC_PER_PRIM = 0.5
->>>>>>> 56091536c423809dad11d3257ce0fe4890cf4ae3
 PROB_NONE_SECONDARY = 0
 
 #________ For Real Data ________#
@@ -55,7 +50,7 @@ UPDATE_ON_CONSENSUS = False #decides whether the task statistics are updated for
 #_______ Selectivity and Ambiguity Settings _________#
 JF_AMBIGUITY = 0.5
 JF_SELECTIVITY = 0.1
-SEC_PRED_SELECTIVITY = 0.3
+SEC_PRED_SELECTIVITY = 0.5
 SEC_PRED_AMBIGUITY = 0
 JOIN_COND_AMBIGUITY = 0
 JOIN_COND_SELECTIVITY = 0.3 # given that these pairs were created by the crowd
