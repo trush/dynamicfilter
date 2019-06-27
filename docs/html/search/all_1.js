3f5a73e1ea64c6e6b182567471e0d8b96d989288
Summer 19 <summer19@Summers-Mac-mini.local>
2019-06-27 10:19:23 -0700

    organized

---
+++ docs/html/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['installation_20guide',['Installation Guide',['../install_info.html',1,'']]],
+  ['information_20concerning_20_22toggles_2epy_22_20file',['Information Concerning &quot;toggles.py&quot; File',['../toggles.html',1,'']]]
+];
