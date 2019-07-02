a5f3894449bdc87928478d5f89b84e40f24e872c
Summer 19 <summer19@Summers-Mac-mini.local>
2019-07-02 10:44:44 -0700

    print statements removed

--- joinapp/join_simulations.py
+++ joinapp/join_simulations.py
@@ -562,7 +562,7 @@ class JoinSimulation():
             elif JOIN_TYPE is 2:
                 task = choose_task_PJF(worker_id, estimator)
             elif JOIN_TYPE is 3:
-                task = choose_task_IWS2(worker_id, estimator)
+                task = choose_task_IWS1(worker_id, estimator)
 
     
             if type(task) is JFTask:
